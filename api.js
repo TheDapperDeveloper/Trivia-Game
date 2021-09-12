ff100a1e7428460c4efaacb39818231c9f358916
Brande Gilbert <brandegilbert@gmail.com>
2021-09-12 17:16:58 -0400

    Question and answers on the screen. Need a way to show the correct answer

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const searchURL = "https://opentdb.com/api.php?amount=50&category=11&difficulty=
 async function generateQuestion () {
     const mainContainer = document.querySelector(".main-container")
     const secContainer = document.querySelector(".sec-container")
+    const optContainer = document.querySelector("option-container")
     const choiceContainer = document.createElement("div")
     choiceContainer.class = "choices"
     const answerContainer = document.createElement("div")
@@ -13,28 +14,32 @@ async function generateQuestion () {
     const generateQuestion = await fetch(`${searchURL}`)
     const jsonQuestion = await generateQuestion.json();
 
-    console.table(jsonQuestion.results[3].question)
-    console.table(jsonQuestion.results[3].incorrect_answers)
-    console.table(jsonQuestion.results[3].correct_answer)
     mainContainer.append(secContainer)
     secContainer.append(jsonQuestion.results[3].question)
     
-    const ansData = jsonQuestion.results[3].incorrect_answers;
-    console.log(jsonQuestion.results[3].incorrect_answers)
-    const ansString = ''
-    for (const answer of ansData) {
-        ansString += `<ol>`;
-        ansData += `<li>${answer}</li>`
-        ansString +=`</ol>`;
-    }
-    
-    
-    mainContainer.append(choiceContainer)
-    choiceContainer.append(ansString)
-    choiceContainer.append(jsonQuestion.results[3].incorrect_answers)
-    choiceContainer.append(jsonQuestion.results[3].correct_answer)
-    
+    const optData = jsonQuestion.results[3].incorrect_answers;
+    const options = document.getElementById("option-container");
+    optData.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerText = item;
+        options.appendChild(li);  
+        mainContainer.append(optContainer)
+    });
+
+    const ansData = jsonQuestion.results[3].correct_answer;
+    const answer = document.getElementById("option-container");
+    const newLi = document.createElement("li")
+    newLi.innerText = ansData
+    answer.appendChild(newLi)
+
 }
+    
+    // mainContainer.append(choiceContainer)
+    // choiceContainer.append(answers)
+    // choiceContainer.append(jsonQuestion.results[3].incorrect_answers)
+    // choiceContainer.append(jsonQuestion.results[3].correct_answer)
+    
+
 
 
 
